2fca377eb5ca22df1d62652e64bd7ec6175cb3cd
franciscafnds <francisca.fernandes.almeida@gmail.com>
2024-09-27 22:40:22 +0200

    fin du tp1 encore ...

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -11,10 +11,12 @@ public class Gaulois {
 		this.force = force;
 	}
 
+	// Getter pour le nom
 	public String getNom() {
 		return nom;
 	}
 
+	// Methode pour parler
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "<<" + texte + ">>");
 	}
@@ -23,19 +25,33 @@ public class Gaulois {
 		return "Le gaulois " + nom + " : ";
 	}
 
+	// Methode pour frapper un ennemi
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
 		romain.recevoirCoup(force / 3);
 	}
+	private char[] recevoirCoup() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-//	@Override
-//	public String toString() {
-//		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-//	}
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
 
 	public static void main(String[] args) {
-		
-		System.out.println(asterix);
+		// Verification de prendreParole
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix.prendreParole());
+
+		// Verification de parler
+		asterix.parler("Bonjour tout le monde !");
+
+		// Verification de frapper
+		asterix.frapper("un romain");
 
 	}
+
+
 }
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,24 +3,24 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
+
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	
+
 	public string getNom() {
 		return nom;
 	}
-	
+
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "<<" + texte + ">>");
 	}
-	
+
 	private String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
-	
+
 	public void recevoirCoup(int forceCoup) {
 		force -= forceCoup;
 		if (force > 0) {
@@ -28,7 +28,11 @@ public class Romain {
 		} else {
 			parler("J'abandonne...");
 		}
+
+	}
+
+	public static void main(String[] args) {
 		
 	}
-	
+
 }
