622327e0dc81399f080b1b2768c8f35bd61276b7
franciscafnds <francisca.fernandes.almeida@gmail.com>
2024-10-06 17:15:24 +0200

    c'est la fin du tp1

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -1,5 +1,6 @@
 package histoire;
 
+import personnages.Druide;
 import personnages.Gaulois;
 import personnages.Romain;
 
@@ -8,13 +9,21 @@ public class Scenario {
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		Romain minus = new Romain("Minus", 6);
-		
-		asterix.parler("Bonjour tout le monde !");
+		Gaulois obelix = new Gaulois("Obelix", 25);
+		Druide panoramix = new Druide("Panoramix", 5, 10);
+
+	
+		panoramix.parler(" Je vais aller préparer une petite potion...");
+		panoramix.preparerPotion();
+		panoramix.booster(obelix);
+
+		obelix.parler("Par Bélénos, ce n'est pas juste !");
+		asterix.boirePotion(6);
+		asterix.parler("Bonjour");
+
 		minus.parler("UN GAU... UN GAUGAU...");
-		
 		asterix.frapper(minus);
-		asterix.frapper(minus);
-		asterix.frapper(minus);
+
 
 	}
 
