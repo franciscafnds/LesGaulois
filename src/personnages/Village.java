8b0cf54de8d2bc6b4961f2b09e091d76b7b1a81d
franciscafnds <francisca.fernandes.almeida@gmail.com>
2024-10-17 19:48:00 +0200

    tp2_debut...

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,19 +1,45 @@
+
 package personnages;
 
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
 
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
-
+	
 	public String getNom() {
 		return nom;
 	}
 	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois]=gaulois;
+	}
+	
+	public Gaulois trouverHabitant(int ref) {
+		return villageois[ref];
+	}
+
+	
 	public void setChef(Chef chef) {
 		this.chef = chef;
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles",30);
+		//Gaulois gaulois = village.trouverHabitant(30);
+		//e) le tableau à une taille de 0 à 29 l'emplacement 30 est donc inutilisable.
+		Chef abararacourcix = new Chef("Abararacourcix",6,village);
+		village.setChef(abararacourcix);
+		Gaulois asterix = new Gaulois("Astérix",8);
+		village.ajouterHabitant(asterix);
+		//Gaulois gaulois = village.trouverHabitant(1);
+		//System.out.println(gaulois);
+		//e) asterix est assigné à villageois[0], villageois[1] est donc vide ce qui explique le null
 	}
 
 
