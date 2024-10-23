65e4fd9c2ac7654c9bc3a434d5c68c684bb8d787
franciscafnds <francisca.fernandes.almeida@gmail.com>
2024-10-23 23:57:58 +0200

    il manque derniere question tp2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,8 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -22,14 +24,23 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		int forceSur = force;
 		force -= forceCoup;
+		assert forcePositif();
 		if (force > 0) {
 			parler("Aie");
 		} else {
 			parler("J'abandonne...");
 		}
+		assert force< forceSur;
 
 	}
+	
+	public boolean forcePositif() {
+		return force >=0;
+	}
+	
+	
 	
 	@Override
 	public String toString() {
@@ -39,14 +50,15 @@ public class Romain {
 
 	public static void main(String[] args) {
 		// Verification de prendreParole
-		Romain minus =  new Romain ("Minus", 5);
-		System.out.println(minus);
+		Romain minus =  new Romain ("Minus", 6);
+		System.out.println(minus.prendreParole());
 		
 		// Verification de parler
 		minus.parler("Bonjour tout le monde !");
 		
 		// Verification de recevoir coup
 		minus.recevoirCoup(7);
+		
 		
 	}
 
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -16,12 +16,23 @@ public class Village {
 	public String getNom() {
 		return nom;
 	}
+	
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
 	public Chef getChef() {
 		return chef;
 	}
 
 	
 	public void ajouterHabitant(Gaulois gaulois) {
+		if (nbVillageois > villageois.length) {
+			System.out.println("Village plein !");
+		} else {
+			villageois[nbVillageois] = gaulois;
+			nbVillageois++;
+		}
 		villageois[nbVillageois]=gaulois;
 	}
 	
@@ -29,11 +40,7 @@ public class Village {
 		return villageois[ref];
 	}
 
-	
-	public void setChef(Chef chef) {
-		this.chef = chef;
-	}
-	
+
 	public void afficherVillageois() {
 		System.out.println("Dans le " + getNom() + " du chef " + getChef() + " vivent les légendaires gaulois :");
 		for (int i=0; i<nbVillageois;i++) {
@@ -41,17 +48,22 @@ public class Village {
 		}
 	}
 	
+	@Override
+	public String toString() {
+		return nom;
+	}
+	
 	public static void main(String[] args) {
 		Village village = new Village("Village des Irréductibles",30);
-		//Gaulois gaulois = village.trouverHabitant(30);
-		//e) le tableau à une taille de 0 à 29 l'emplacement 30 est donc inutilisable.
+		// Gaulois gaulois = village.trouverHabitant(30);
+		// e) le tableau à une taille de 0 à 29 l'emplacement 30 est donc inutilisable.
 		Chef abararacourcix = new Chef("Abararacourcix",6,village);
 		village.setChef(abararacourcix);
 		Gaulois asterix = new Gaulois("Astérix",8);
 		village.ajouterHabitant(asterix);
-		//Gaulois gaulois = village.trouverHabitant(1);
-		//System.out.println(gaulois);
-		//e) asterix est assigné à villageois[0], villageois[1] est donc vide ce qui explique le null
+		// Gaulois gaulois = village.trouverHabitant(1);
+		// System.out.println(gaulois);
+		// e) asterix est assigné à villageois[0], villageois[1] est donc vide ce qui explique le null
 		Gaulois obelix = new Gaulois("Obélix",25);
 		village.ajouterHabitant(obelix);
 		village.afficherVillageois();
